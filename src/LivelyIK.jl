07e2c50680944ee4a340be01439fb9bfa3c7b97f
Andrew Schoen <schoen.andrewj@gmail.com>
2020-08-12 16:53:57 -0500

    fixed imports and paths in julia

--- src/LivelyIK.jl
+++ src/LivelyIK.jl
@@ -2,6 +2,7 @@ module LivelyIK
 
 using PyCall
 collision_transfer = pyimport("lively_ik.utils.collision_transfer")
+lively_ik = pyimport("lively_ik")
 
 include("groove/groove.jl")
 include("spacetime/spacetime.jl")
