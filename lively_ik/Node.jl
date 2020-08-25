da805933ff0c2255a41518f8c88c5589ea033a6b
Andrew Schoen <schoen.andrewj@gmail.com>
2020-08-25 08:38:27 -0500

    relocated and reconfigured goals

--- lively_ik/Node.jl
+++ lively_ik/Node.jl
@@ -32,68 +32,68 @@ info_string = response.values[1].string_value
 output_topic = response.values[2].string_value
 global info_data = YAML.load(info_string)
 
-function msg_to_args(goal_msg)
-    goal_positions = []
-    goal_quats = []
-
-    # Create POS/QUAT Goals from EE Poses
-    for i = 1:length(goal_msg.ee_poses)
-        p = goal_msg.ee_poses[i]
-
-        pos_x = p.position.x
-        pos_y = p.position.y
-        pos_z = p.position.z
-
-        quat_w = p.orientation.w
-        quat_x = p.orientation.x
-        quat_y = p.orientation.y
-        quat_z = p.orientation.z
-
-        push!(goal_positions, [pos_x, pos_y, pos_z])
-        push!(goal_quats, Quat(quat_w, quat_x, quat_y, quat_z))
-    end
-
-    # Create DC Goals from DC Values
-    dc_goals = goal_msg.dc_values
-
-    # Get Time from Header
-    time = rclpy_time.Time.from_msg(goal_msg.header.stamp).nanoseconds * 10^-9
-
-    # Extract Bias
-    bias = [goal_msg.bias.x,goal_msg.bias.y,goal_msg.bias.z]
-
-    # Extract Weights
-    weights = goal_msg.objective_weights
-
-    return goal_positions, goal_quats, dc_goals, time, bias, weights
-end
-
-function get_initial_goals(lively_ik,info_data)
-    num_chains = lively_ik.relaxedIK_vars.robot.num_chains
-
-    goal_positions = []
-    goal_quats = []
-
-    # Create POS/QUAT Goals from EE Poses
-    for i = 1:length(num_chains)
-        push!(goal_positions, [0.0, 0.0, 0.0])
-        push!(goal_quats, Quat(1.0, 0.0, 0.0, 0.0))
-    end
-
-    # Create DC Goals from DC Values
-    dc_goals = info_data["starting_config"]
-
-    # Get Time from Header
-    time = 0
-
-    # Extract Bias
-    bias = [1,1,1]
-
-    # Extract Weights
-    weights = lively_ik.relaxedIK_vars.vars.weight_priors
-
-    return goal_positions, goal_quats, dc_goals, time, bias, weights
-end
+# function msg_to_args(goal_msg)
+#     goal_positions = []
+#     goal_quats = []
+#
+#     # Create POS/QUAT Goals from EE Poses
+#     for i = 1:length(goal_msg.ee_poses)
+#         p = goal_msg.ee_poses[i]
+#
+#         pos_x = p.position.x
+#         pos_y = p.position.y
+#         pos_z = p.position.z
+#
+#         quat_w = p.orientation.w
+#         quat_x = p.orientation.x
+#         quat_y = p.orientation.y
+#         quat_z = p.orientation.z
+#
+#         push!(goal_positions, [pos_x, pos_y, pos_z])
+#         push!(goal_quats, Quat(quat_w, quat_x, quat_y, quat_z))
+#     end
+#
+#     # Create DC Goals from DC Values
+#     dc_goals = goal_msg.dc_values
+#
+#     # Get Time from Header
+#     time = rclpy_time.Time.from_msg(goal_msg.header.stamp).nanoseconds * 10^-9
+#
+#     # Extract Bias
+#     bias = [goal_msg.bias.x,goal_msg.bias.y,goal_msg.bias.z]
+#
+#     # Extract Weights
+#     weights = goal_msg.objective_weights
+#
+#     return goal_positions, goal_quats, dc_goals, time, bias, weights
+# end
+#
+# function get_initial_goals(lively_ik,info_data)
+#     num_chains = lively_ik.relaxedIK_vars.robot.num_chains
+#
+#     goal_positions = []
+#     goal_quats = []
+#
+#     # Create POS/QUAT Goals from EE Poses
+#     for i = 1:length(num_chains)
+#         push!(goal_positions, [0.0, 0.0, 0.0])
+#         push!(goal_quats, Quat(1.0, 0.0, 0.0, 0.0))
+#     end
+#
+#     # Create DC Goals from DC Values
+#     dc_goals = info_data["starting_config"]
+#
+#     # Get Time from Header
+#     time = 0
+#
+#     # Extract Bias
+#     bias = [1,1,1]
+#
+#     # Extract Weights
+#     weights = lively_ik.relaxedIK_vars.vars.weight_priors
+#
+#     return goal_positions, goal_quats, dc_goals, time, bias, weights
+# end
 
 # Create JS Publisher
 global solutions_pub = node.create_publisher(sensor_msgs.JointState,output_topic,5)
@@ -119,24 +119,19 @@ function goal_cb(goal_msg)
     global lik
     global xopt
     # Handle the goal message and publish the solution from lively_ik
-    goal_positions, goal_quats, dc_goals, time, bias, weights = msg_to_args(goal_msg)
+    goals = LivelyIK.Goals(goal_msg)
+    println("GP (CP): $goals")
     # println("POS: $time $goal_positions")
-    xopt = LivelyIK.solve(lik, goal_positions, goal_quats, dc_goals, time, bias, weights)
+    xopt = LivelyIK.solve(lik, goals.positions, goals.quats, goals.dc, goals.time, goals.bias, goals.weights)
     # println("In CB: $xopt")
 end
 
 # Solve once
 println("Finishing Compilation")
-goal_positions, goal_quats, dc_goals, time, bias, weights = get_initial_goals(lik,info_data)
-println("GP: $goal_positions")
-sol = LivelyIK.solve(lik, goal_positions, goal_quats, dc_goals, time, bias, weights)
-println(goal_positions[1][2])
-goal_positions[1][2] += Float64(0.25)
-println("GP: $goal_positions")
-sol = LivelyIK.solve(lik, goal_positions, goal_quats, dc_goals, time, bias, weights)
-goal_positions[1][2] -= Float64(0.45)
-println("GP: $goal_positions")
-sol = LivelyIK.solve(lik, goal_positions, goal_quats, dc_goals, time, bias, weights)
+goals = LivelyIK.Goals(lik,info_data)
+println("GP (N): $goals")
+sol = LivelyIK.solve(lik, goals.positions, goals.quats, goals.dc, goals.time, goals.bias, goals.weights)
+println("SOL: $sol")
 
 goal_sub = node.create_subscription(wisc_msgs.LivelyGoals,"/robot_goals",goal_cb,5)
 
--- src/LivelyIK.jl
+++ src/LivelyIK.jl
@@ -9,6 +9,80 @@ include("spacetime/spacetime.jl")
 include("utils/utils.jl")
 include("relaxed_ik/relaxed_ik.jl")
 
-export RelaxedIK, get_standard, solve
+struct Goals
+    positions::Array{Array{Float64,3}}
+    quats    ::Array{Array{Float64,4}}
+    dc       ::Array{Float64}
+    time     ::Float64
+    bias     ::Array{Float64,3}
+    weights  ::Array{Float64}
+end
+
+function Goals(goal_msg)
+    positions = []
+    quats = []
+
+    # Create POS/QUAT Goals from EE Poses
+    for i = 1:length(goal_msg.ee_poses)
+        p = goal_msg.ee_poses[i]
+
+        pos_x = p.position.x
+        pos_y = p.position.y
+        pos_z = p.position.z
+
+        quat_w = p.orientation.w
+        quat_x = p.orientation.x
+        quat_y = p.orientation.y
+        quat_z = p.orientation.z
+
+        push!(positions, [pos_x, pos_y, pos_z])
+        push!(quats, Quat(quat_w, quat_x, quat_y, quat_z))
+    end
+
+    # Create DC Goals from DC Values
+    dc = goal_msg.dc_values
+
+    # Get Time from Header
+    time = rclpy_time.Time.from_msg(goal_msg.header.stamp).nanoseconds * 10^-9
+
+    # Extract Bias
+    bias = [goal_msg.bias.x,goal_msg.bias.y,goal_msg.bias.z]
+
+    # Extract Weights
+    weights = goal_msg.objective_weights
+
+    return Goals(positions, quats, dc, time, bias, weights)
+end
+
+function Goals(lively_ik,info_data)
+    num_chains = lively_ik.relaxedIK_vars.robot.num_chains
+
+    positions = []
+    quats = []
+
+    # Create POS/QUAT Goals from EE Poses
+    for i = 1:length(num_chains)
+        push!(positions, [0.0, 0.0, 0.0])
+        push!(quats, Quat(1.0, 0.0, 0.0, 0.0))
+    end
+
+    # Create DC Goals from DC Values
+    dc = info_data["starting_config"]
+
+    # Get Time from Header
+    time = 0.0
+
+    # Extract Bias
+    bias = [1.0,1.0,1.0]
+
+    # Extract Weights
+    weights = lively_ik.relaxedIK_vars.vars.weight_priors
+
+    return Goals(positions, quats, dc, time, bias, weights)
+end
+
+
+
+export RelaxedIK, get_standard, solve, Goals
 
 end # module
