7a2d149c497db05e8cd936885956dfb7c325ea6d
Andrew Schoen <schoen.andrewj@gmail.com>
2020-08-25 10:00:55 -0500

    Update LivelyIK.jl

--- src/LivelyIK.jl
+++ src/LivelyIK.jl
@@ -3,6 +3,7 @@ module LivelyIK
 using PyCall
 collision_transfer = pyimport("lively_ik.utils.collision_transfer")
 lively_ik = pyimport("lively_ik")
+rclpy_time = pyimport("rclpy.time")
 
 include("groove/groove.jl")
 include("spacetime/spacetime.jl")
