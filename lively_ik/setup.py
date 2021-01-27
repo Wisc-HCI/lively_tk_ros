0f768308b9830f7db3b1f6c198b34615b907d315
Andrew Schoen <schoen.andrewj@gmail.com>
2021-01-27 09:27:33 -0600

    tweak to requirements

--- lively_ik/setup.py
+++ lively_ik/setup.py
@@ -44,7 +44,7 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=data_files,
-    install_requires=['setuptools','sklearn','numpy','fcl','urdf-parser-py','PyKDL'],
+    install_requires=['setuptools','sklearn','numpy','python-fcl','urdf-parser-py'],
     zip_safe=True,
     maintainer='schoen',
     maintainer_email='schoen.andrewj@gmail.com',
